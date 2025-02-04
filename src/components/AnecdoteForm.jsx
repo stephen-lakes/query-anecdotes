e073d9d0f4a6ffbfca15ad9bc65e3295dd9a3e40
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-04 11:52:17 +0100

    Add new Anecdote
    Use mututation to create a new Anecdote

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,21 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+import { addNewAnecdote } from "../requests";
+
 const AnecdoteForm = () => {
+  const addNewAnecdoteMutation = useMutation({
+    mutationFn: addNewAnecdote,
+  });
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    addNewAnecdoteMutation.mutate({ content, votes: 0 });
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
