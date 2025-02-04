cb7961bd6ed86042d0a538fdc57fdf8a71324a2c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-04 11:18:59 +0100

    fetch data from json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+  });
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+  // const handleVote = (anecdote) => {
+  //   console.log("vote");
+  // };
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data;
+
+  if (result.isLoading) return <div>Loading...</div>;
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
