aca35e33cb29cf0d8b5997ee6b10d1596bfef63f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-04 14:06:41 +0100

    feat: Implement counter functionality using useReducer
    - Added counterReducer function with actions INC, DEC, and ZERO
    - Utilized useReducer hook in App component to manage counter state
    - Created buttons to dispatch respective actions and update counter
      state

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,29 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useReducer } from "react";
 
-import AnecdoteForm from "./components/AnecdoteForm";
-import Notification from "./components/Notification";
-import ErrorPage from "./components/ErrorPage";
-import { getAnecdotes, updateAnecdote } from "./requests";
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      return state;
+  }
+};
 
 const App = () => {
-  const queryClient = useQueryClient();
-
-  const {
-    isLoading,
-    isError,
-    data: anecdotes,
-    error,
-  } = useQuery({
-    queryKey: ["anecdotes"],
-    queryFn: getAnecdotes,
-    retry: 1,
-  });
-
-  const updateAnecdoteMutation = useMutation({
-    mutationFn: updateAnecdote,
-    onSuccess: (updatedAnecdote) => {
-      queryClient.invalidateQueries(["anecdotes"]);
-      const anecdotes = queryClient.getQueryData(["anecdotes"]);
-      queryClient.setQueryData(
-        ["anecdotes"],
-        anecdotes.map((anecdote) =>
-          anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote
-        )
-      );
-    },
-  });
-
-  const handleVote = (anecdote) => {
-    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
-  };
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (isError) return <ErrorPage message={error.message} />;
+  const [counter, counterDispatch] = useReducer(counterReducer, 0);
 
   return (
     <div>
-      <h3>Anecdote app</h3>
-
-      <Notification />
-      <AnecdoteForm />
-
-      {anecdotes && anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <div>{counter}</div>
+      <div>
+        <button onClick={() => counterDispatch({ type: "INC" })}>+</button>
+        <button onClick={() => counterDispatch({ type: "DEC" })}>-</button>
+        <button onClick={() => counterDispatch({ type: "ZERO" })}>0</button>
+      </div>
     </div>
   );
 };
