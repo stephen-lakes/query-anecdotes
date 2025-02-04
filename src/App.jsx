6b91d410de756bb00c0f851f942f9e31abcf1597
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-04 11:21:34 +0100

    Handle fetch errors
    Display the right error mesaage when the request fails

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const result = useQuery({
     queryKey: ["anecdotes"],
     queryFn: () =>
-      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+      axios.get("http://localhost:3001/anecdote").then((res) => res.data),
   });
 
   // const handleVote = (anecdote) => {
@@ -18,6 +18,8 @@ const App = () => {
   const anecdotes = result.data;
 
   if (result.isLoading) return <div>Loading...</div>;
+
+  if (result.isError) return <div>{result.error.message}</div>;
 
   return (
     <div>
